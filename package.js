66f69a57180288f9b8db8c19fa334beab9b1bce5
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:14:31 -0500

    Updated component to version 1.9.0

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-rail',
+  summary : 'Semantic UI - Rail (official): Single component release of rail',
+  version : '1.9.0',
+  git     : 'git://github.com/Semantic-Org/UI-Rail.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
